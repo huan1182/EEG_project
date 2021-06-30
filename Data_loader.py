5516d166767989e0981961cffd7d383919008099
huan1182 <32281885+huan1182@users.noreply.github.com>
2021-06-29 23:09:00 -0400

    Add files via upload

---
+++ Data_loader.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 18:35:25 2021
+
+@author: Huyunting Huang
+"""
+
+import pandas as pd
+
+def data_create(path):
+    data=pd.read_csv(path)
+    data.columns=['time', 'chan1','chan2','chan3','chan4','chan5','chan6','chan7','chan8']
+    return data
+
+
+def address_reader(pre,research_num=1,day_num=1):
+    name=pre+"/Research "+str(research_num)+"/Day "+str(day_num)+"/Day "+str(day_num)+".csv"
+    return name
+def add_label(length,label,data):
+    label0= pd.DataFrame(label, columns=['label'])
+    data['label']=label0
+    return data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
